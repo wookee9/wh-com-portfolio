1cd11f7855803dcab8cdac065466edc8cbaf6b75
William Hooke <wrhooke@gmail.com>
2019-08-22 09:34:04 +0100

    wip background image loading

--- src/content-modules/WorkModule.jsx
+++ src/content-modules/WorkModule.jsx
@@ -1,30 +1,51 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 import SEO from '../components/seo';
 import ModuleLayout from '../components/ModuleLayout';
-import ProjectLightbox from '../components/ProjectLightbox';
+// import ProjectLightbox from '../components/ProjectLightbox';
 
 const WorkModule = () => {
-  const {
-    allContentfulProject: {
-      projects,
-    },
-  } = useStaticQuery(graphql`
+  // const {
+  //   allContentfulProject: {
+  //     projects,
+  //   },
+  // } = useStaticQuery(graphql`
+  //   query {
+  //     allContentfulProject {
+  //       projects: edges {
+  //         project: node {
+  //           title
+  //           key
+  //           client
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
+
+  const thumbs = useStaticQuery(graphql`
     query {
       allContentfulProject {
-        projects: edges {
-          project: node {
+        nodes {
+          thumbnails {
+            fluid(maxWidth: 1000) {
+              ...GatsbyContentfulFluid
+            }
             title
-            key
-            client
           }
         }
       }
     }
   `);
 
-  const spyscape = projects.find(({ project: { key } }) => key === 'spyscape').project;
-  console.log(spyscape);
+  console.log(thumbs);
+
+  const thumbImageData = thumbs.allContentfulProject.nodes[0].thumbnails[0].fluid;
+
+  console.log(thumbImageData);
+  // const spyscape = projects.find(({ project: { key } }) => key === 'spyscape').project;
+  // console.log(spyscape);
 
   return (
     <ModuleLayout id="work">
@@ -33,6 +54,17 @@ const WorkModule = () => {
         keywords={['william hooke', 'full stack', 'engineer', 'react', 'london', 'work']}
       />
       <h2>Selected Work</h2>
+
+      <BackgroundImage
+        // Tag="div"
+        // className={className}
+        fluid={thumbImageData}
+        backgroundColor="#040e18"
+        width="400"
+        height="400"
+      >
+        <h1>Hello gatsby-background-image</h1>
+      </BackgroundImage>
       {
         // projects.map(({ project }) => (
         //   <ProjectLightbox
@@ -41,10 +73,10 @@ const WorkModule = () => {
         //   />
         // ))
       }
-      <ProjectLightbox
+      {/* <ProjectLightbox
         {...spyscape}
         projectKey={spyscape.key}
-      />
+      /> */}
     </ModuleLayout>
   );
 };
